8037ca8df8dae2d31a34983537bf8a925e1ec7f5
Mariano Medina <nanomed9@ca.frre.utn.edu.ar>
2024-08-20 09:00:01 -0300

    act

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5912,8 +5912,87 @@
   addEventListener('resize', removeStyle)
 
 
-
+  var swiperAutos = new Swiper('#autos', {
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+    
+    spaceBetween: 10,
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
   
+  var swiperPickups = new Swiper('#pickups', {
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+    },
+    
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  var swiperCross = new Swiper('#cross', {
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+    },
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
+
   
 // Tab functionality with animations
 function showTab(tabId) {
