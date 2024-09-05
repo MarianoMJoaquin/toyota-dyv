495ff56eea69d7def210e26eadd8861ebd937b64
Mariano Medina <nanomed9@ca.frre.utn.edu.ar>
2024-09-05 18:51:19 -0300

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 
 
+import netlify from '@astrojs/netlify';
+
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: netlify()
 });
