ccb342e78cd162536e4e8842c9bd856b76d95f8c
Mariano Medina <nanomed9@ca.frre.utn.edu.ar>
2024-10-07 17:30:02 -0300

    mismo que el anterior

--- src/components/autoDetalle.jsx
+++ src/components/autoDetalle.jsx
@@ -19,6 +19,8 @@ import "swiper/css/zoom";
 
 import "../assets/styles/swiperAutoDetalles.css";
 
+import UCTLogo from "../../src/assets/images/usado.png";
+
 import GoogleMap from "./GoogleMap.jsx";
 
 export default function AutoDetalles({ slug }) {
@@ -178,7 +180,7 @@ export default function AutoDetalles({ slug }) {
                     />
                     {detallesAuto.uct === 1 ? (
                       <img
-                        src="../../src/assets/images/usado.png"
+                        src={UCTLogo}
                         alt="UCT"
                         className="w-16 max-sm:w-11 absolute top-4 right-4"
                       />
