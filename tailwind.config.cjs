2754c7112515dfd9e203b39e1ddb7c5709fab4c4
Mariano Medina <nanomed9@ca.frre.utn.edu.ar>
2025-01-06 08:25:28 -0300

    act

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'toyota-red': {
+          DEFAULT: '#EB0A1E',
+          '10': 'rgba(235, 10, 30, 0.1)',
+          '20': 'rgba(235, 10, 30, 0.2)',
+          '40': 'rgba(235, 10, 30, 0.4)',
+        }
+      }
+    },
   },
   plugins: [],
 }
