7a6e6c9a5c71a14a9ea456639dfe318929fdd3f1
Mariano Medina <nanomed9@ca.frre.utn.edu.ar>
2025-01-13 10:39:59 -0300

    Hoaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 
-<<<<<<< HEAD
 
 
 
 
-=======
->>>>>>> b1500ef4e815c5c672409b6207e16d7c64a36741
 import react from '@astrojs/react';
 import node from '@astrojs/node';
 
