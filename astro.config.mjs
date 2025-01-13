5b0c477d760938da57d1c932759bc4fd13dd86fe
Mariano Medina <nanomed9@ca.frre.utn.edu.ar>
2025-01-13 10:44:38 -0300

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://test-website.derkayvargas.com',
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: node({
